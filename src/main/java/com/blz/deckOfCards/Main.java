c4b03064a89385c43d9acf73ced9e2ae1f77aca3
nayab jahan <nayabzafir@gmail.com>
2022-08-18 03:05:09 +0530

    Added:DeckOfCards

--- src/main/java/com/blz/deckOfCards/Main.java
+++ src/main/java/com/blz/deckOfCards/Main.java
@@ -3,6 +3,12 @@ package com.blz.deckOfCards;
 public class Main {
     public static void main(String[] args) {
         System.out.println("object oriented programmes");
+        System.out.println(".......Deck Of Cards.......");
+        Card card = new Card();
+        card.shuffleCard();
+        card.assignCards();
+        card.display();
     }
 }
+
 
